0d1b4340490ac282a967153bc8ebc5ccef529339
John Harrington <jharringtonmn@icloud.com>
2025-04-22 06:42:53 -0700

    before new format

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 import re
 from PyPDF2 import PdfReader
 
-pdf_file = 'docs/1_6_23.pdf' 
+pdf_files = ['docs/7_18_23.pdf']
 results_pattern = r"^(?!0\d+$)0?$|^[1-9]\d*(\.\d+)?$|^0\.\d+$"
 
 def extract_collected_date(text):
-    for line in text: 
+    for line in text:
         if line.startswith("COLLECTED:"):
             date_pattern = r"^COLLECTED:\s*(0[1-9]|1[0-2])/(0[1-9]|[12]\d|3[01])/(\d{4})"
             match = re.match(date_pattern, line)
             if match:
-                print(f"date 1: {match.group(1)}")
-                print(f"date 2: {match.group(2)}")
-                print(f"date 3: {match.group(3)}")
-
                 return f"{match.group(1)}/{match.group(2)}/{match.group(3)}"
+        elif "Collection Date/Time:" in line:
+            date_pattern = r"Collection Date/Time:\s*(\d{2}/\d{2}/\d{4})"
+            match = re.search(date_pattern, line)
+            if match:
+                return match.group(1)
     return None
 
 def extract_lines_from_pdf(pdf_path):
@@ -30,26 +31,38 @@ def extract_lines_from_pdf(pdf_path):
                     all_lines.extend(lines)
     except FileNotFoundError:
         return f"Error: File not found at '{pdf_path}'"
+    
+    for line in all_lines:
+        print(line)
     return all_lines
 
 if __name__ == "__main__":
     results = {}
-    extracted_lines = extract_lines_from_pdf(pdf_file)
-    if isinstance(extracted_lines, list):
-        date = extract_collected_date(extracted_lines)
-        if date is None: 
-            print("Error collecting date for file <get filename>")
+    for file in pdf_files:
+        extracted_lines = extract_lines_from_pdf(file)
+        if isinstance(extracted_lines, list):
+            date = extract_collected_date(extracted_lines)
+            if date is None: 
+                print("Error collecting date for file <get filename>")
+            else:
+                print(f"Reading results collected on {date}")
 
-        last_line = ""
-        for line in extracted_lines:
-            match = re.match(results_pattern, line)
-            if match:
-                results[last_line] = (line, date)
+            last_line = ""
+            for line in extracted_lines:
+                match = re.match(results_pattern, line)
+                if match:
+                    if last_line in ["01", " of "] or last_line.startswith("MD"):
+                        continue
 
-            last_line = line
-            #print(line)
-        for key, val in results.items():
-            print(f"{key}:\t{val}")
+                    if last_line not in results:
+                        results[last_line] = [] 
 
-    else:
-        print(extracted_lines)
+                    results[last_line].append((line, date))
+
+                last_line = line
+
+        else:
+            print(f"Error extracting <get_filename>: {extracted_lines}")
+    
+    for key, val in results.items():
+        print(f"{key}:\t{val}")
